9525af3a691e7a80521b88bbeac0f07ce954b7d3
mechon03 <amangupta9255@gmail.com>
2022-06-15 12:03:42 +0530

    added influencers list and about guzelgiy

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import Trending from './Trending'
 import TeamList from './TeamList'
+import AboutUs from './AboutUs.js'
+import TrendingInfluencers from './TrendingInfluencers'
 
 function HomePage() {
   return (
     <>
+        <AboutUs />
+        <TrendingInfluencers />
         <Trending/>
         <TeamList/>
     </>
---
+++ src/components/TrendingInfluencers.js
@@ -0,0 +1,14 @@
+import Heading from './InfluencersHeading';
+import Influencer1 from './InfluencersDetails';
+import './TrendingInfluencers.css';
+
+const trendingInfluencers = () => {
+    return ( 
+        <>
+        <Heading />
+        <Influencer1 />
+        </>
+     );
+}
+ 
+export default trendingInfluencers;
